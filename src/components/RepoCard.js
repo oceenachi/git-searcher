20837946d65a7516785491564f474018df626f43
francisca eze <phillipfrancisca@gmail.com>
2021-06-18 11:46:07 +0100

    create dummy repo cards

---
+++ src/components/RepoCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const RepoCard = () => {
+    return (
+        <StyledRepoCard>
+            Hey there its mejoi  ednf
+            gwehfbojmfq9jf
+            qrhuvbjrfoj egqb
+            eqhjfbnf
+            
+        </StyledRepoCard>
+    )
+}
+const StyledRepoCard = styled.div`
+    border: 1px red solid;
+    min-height:  200px;
+
+
+`;
+
+export default RepoCard
