8c6f3ee48b20eb97d5dfca09f479a414de8ae2ca
francisca eze <phillipfrancisca@gmail.com>
2021-06-19 19:27:08 +0100

    add combined reducers

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import repoReducer from "./reducers/repoReducer";
+
+
+const rootReducer = combineReducers({
+    repoReducer
+});
+
+export default rootReducer;
