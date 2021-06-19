dc8d3cd2de69727cc6304f4554945ecbd19f5e0d
francisca eze <phillipfrancisca@gmail.com>
2021-06-19 14:05:56 +0100

    wrapped store across component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Provider } from "react-redux";
 import ParentBody from "./components/ParentBody";
+import { PersistGate } from "redux-persist/integration/react";
+import configStore from "./stateConfig.js/configureStore";
+import persistor from "./stateConfig.js/configureStore";
+import {ScaleLoader} from "react-spinners"
+
 
 function App() {
+
+  //create store and persistor
   return (
-    <div className="App">
-    <ParentBody/>
-    </div>
+    <>
+      <Provider store={configStore.store}>
+        <PersistGate loading={<ScaleLoader/>} persistor={configStore.persistor}>
+          <ParentBody />
+
+        </PersistGate>
+
+
+      </Provider>
+    </>
   );
 }
 
