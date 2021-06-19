552b1ae096532a0765bc1dcc241c6914e9b28556
francisca eze <phillipfrancisca@gmail.com>
2021-06-19 19:26:14 +0100

    create configure store to use redux thunk

---
+++ src/redux/configureStore.js
@@ -0,0 +1,30 @@
+import {applyMiddleware, createStore, compose } from "redux";
+import thunk from 'redux-thunk'
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./rootReducer";
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel2
+}
+
+const PersistedReducer = persistReducer(persistConfig, rootReducer);
+
+const configureStore = (initialState) => {
+    return createStore(PersistedReducer,initialState,composeEnhancers(applyMiddleware(thunk)) )
+}
+
+export default (() => {
+  
+    let store = configureStore();
+
+    let persistor = persistStore(store);
+
+    return {store, persistor}
+})()
