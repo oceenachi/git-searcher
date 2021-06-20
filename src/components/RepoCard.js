4c02c66615200417f126228555f48b302b3e6f77
francisca eze <phillipfrancisca@gmail.com>
2021-06-20 20:52:13 +0100

    add loading states

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -1,38 +1,162 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import { GoEye, GoRepoForked, GoOrganization, GoDiffModified } from "react-icons/go";
 import { AiOutlineStar } from "react-icons/ai";
-
+import { useSelector, useDispatch} from 'react-redux';
+import { getUsers } from '../redux/action';
+import {ScaleLoader} from "react-spinners"; 
 
 
 const RepoCard = ({ item }) => {
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
 
-    const { name, username, description, avatar, fork, contributions, watch, stars, organization, language, license, private: isPrivate, full_name: fullName } =  item;
+    const singleUser = useSelector((state) => state.usersReducer[item.owner.login]);
+    // const loading = useSelector((state) => state.);
+
+    useEffect(() => {
+        if(singleUser === undefined){
+            dispatch(getUsers(item.owner.login, item.owner.url)).then(() => {
+                setLoading(false);
+            });
+        }else{
+            // setLoading(false);
+        }
+
+    }, [])
+    console.log({singleUser});
+
+    /*
+    archive_url: "https://api.github.com/repos/mikegcoleman/todo/{archive_format}{/ref}"
+archived: false
+assignees_url: "https://api.github.com/repos/mikegcoleman/todo/assignees{/user}"
+blobs_url: "https://api.github.com/repos/mikegcoleman/todo/git/blobs{/sha}"
+branches_url: "https://api.github.com/repos/mikegcoleman/todo/branches{/branch}"
+clone_url: "https://github.com/mikegcoleman/todo.git"
+collaborators_url: "https://api.github.com/repos/mikegcoleman/todo/collaborators{/collaborator}"
+comments_url: "https://api.github.com/repos/mikegcoleman/todo/comments{/number}"
+commits_url: "https://api.github.com/repos/mikegcoleman/todo/commits{/sha}"
+compare_url: "https://api.github.com/repos/mikegcoleman/todo/compare/{base}...{head}"
+contents_url: "https://api.github.com/repos/mikegcoleman/todo/contents/{+path}"
+contributors_url: "https://api.github.com/repos/mikegcoleman/todo/contributors"
+created_at: "2018-05-16T03:03:12Z"
+default_branch: "master"
+deployments_url: "https://api.github.com/repos/mikegcoleman/todo/deployments"
+description: "Simple todo app built using Express, Mongo, and Node"
+disabled: false
+downloads_url: "https://api.github.com/repos/mikegcoleman/todo/downloads"
+events_url: "https://api.github.com/repos/mikegcoleman/todo/events"
+fork: false
+forks: 57
+forks_count: 57
+forks_url: "https://api.github.com/repos/mikegcoleman/todo/forks"
+full_name: "mikegcoleman/todo"
+git_commits_url: "https://api.github.com/repos/mikegcoleman/todo/git/commits{/sha}"
+git_refs_url: "https://api.github.com/repos/mikegcoleman/todo/git/refs{/sha}"
+git_tags_url: "https://api.github.com/repos/mikegcoleman/todo/git/tags{/sha}"
+git_url: "git://github.com/mikegcoleman/todo.git"
+has_downloads: true
+has_issues: true
+has_pages: false
+has_projects: true
+has_wiki: true
+homepage: null
+hooks_url: "https://api.github.com/repos/mikegcoleman/todo/hooks"
+html_url: "https://github.com/mikegcoleman/todo"
+id: 133602815
+issue_comment_url: "https://api.github.com/repos/mikegcoleman/todo/issues/comments{/number}"
+issue_events_url: "https://api.github.com/repos/mikegcoleman/todo/issues/events{/number}"
+issues_url: "https://api.github.com/repos/mikegcoleman/todo/issues{/number}"
+keys_url: "https://api.github.com/repos/mikegcoleman/todo/keys{/key_id}"
+labels_url: "https://api.github.com/repos/mikegcoleman/todo/labels{/name}"
+language: "CSS"
+languages_url: "https://api.github.com/repos/mikegcoleman/todo/languages"
+license: null
+merges_url: "https://api.github.com/repos/mikegcoleman/todo/merges"
+milestones_url: "https://api.github.com/repos/mikegcoleman/todo/milestones{/number}"
+mirror_url: null
+name: "todo"
+node_id: "MDEwOlJlcG9zaXRvcnkxMzM2MDI4MTU="
+notifications_url: "https://api.github.com/repos/mikegcoleman/todo/notifications{?since,all,participating}"
+open_issues: 4
+open_issues_count: 4
+owner: {login: "mikegcoleman", id: 6797199, node_id: "MDQ6VXNlcjY3OTcxOTk=", avatar_url: "https://avatars.githubusercontent.com/u/6797199?v=4", gravatar_id: "", …}
+private: false
+pulls_url: "https://api.github.com/repos/mikegcoleman/todo/pulls{/number}"
+pushed_at: "2020-09-06T21:04:37Z"
+releases_url: "https://api.github.com/repos/mikegcoleman/todo/releases{/id}"
+score: 1
+size: 1012
+ssh_url: "git@github.com:mikegcoleman/todo.git"
+stargazers_count: 79
+stargazers_url: "https://api.github.com/repos/mikegcoleman/todo/stargazers"
+statuses_url: "https://api.github.com/repos/mikegcoleman/todo/statuses/{sha}"
+subscribers_url: "https://api.github.com/repos/mikegcoleman/todo/subscribers"
+subscription_url: "https://api.github.com/repos/mikegcoleman/todo/subscription"
+svn_url: "https://github.com/mikegcoleman/todo"
+tags_url: "https://api.github.com/repos/mikegcoleman/todo/tags"
+teams_url: "https://api.github.com/repos/mikegcoleman/todo/teams"
+trees_url: "https://api.github.com/repos/mikegcoleman/todo/git/trees{/sha}"
+updated_at: "2021-04-24T15:18:57Z"
+url: "https://api.github.com/repos/mikegcoleman/todo"
+watchers: 79
+watchers_count: 79
+
+
+
+avatar_url: "https://avatars.githubusercontent.com/u/6797199?v=4"
+events_url: "https://api.github.com/users/mikegcoleman/events{/privacy}"
+followers_url: "https://api.github.com/users/mikegcoleman/followers"
+following_url: "https://api.github.com/users/mikegcoleman/following{/other_user}"
+gists_url: "https://api.github.com/users/mikegcoleman/gists{/gist_id}"
+gravatar_id: ""
+html_url: "https://github.com/mikegcoleman"
+id: 6797199
+login: "mikegcoleman"
+node_id: "MDQ6VXNlcjY3OTcxOTk="
+organizations_url: "https://api.github.com/users/mikegcoleman/orgs"
+received_events_url: "https://api.github.com/users/mikegcoleman/received_events"
+repos_url: "https://api.github.com/users/mikegcoleman/repos"
+site_admin: false
+starred_url: "https://api.github.com/users/mikegcoleman/starred{/owner}{/repo}"
+subscriptions_url: "https://api.github.com/users/mikegcoleman/subscriptions"
+type: "User"
+url: "https://api.github.com/users/mikegcoleman"
+    */
+
+    console.log(item)
+    const { name, username, description, avatar, fork, contributions, watch, stars, organization, language, license, private: isPrivate, full_name: fullName, owner } =  item;
+
+    useEffect(() => {
+
+    }, [loading])
     return (
-        <StyledRepoCard>
+        <StyledRepoCard avatar={owner.avatar_url}>
             <div className="card-repo-type">
-                {isPrivate ? "Private" : "Public"}
+             {isPrivate ? "Private" : "Public"}
             </div>
+        { loading ? <ScaleLoader/> :   
+        
             <div className="repo-content">
                 <div className="repo-bio">
                     <div className="repo-avatar"></div>
                     <div className="repo-title">
                         <a href={"#"}>
                             <h1>
-                                {fullName || "decagonhq/fellowship-FE"}
+                                {name || ""}
                             </h1>
                         </a>
-                        <p>Created by <a href={"#"}>{avatar || "@avatar"}</a></p>
+                        <p>Created by <a href={"#"}>{(singleUser||owner).login || ""}</a></p>
                     </div>
                 </div>
                 <p className="repo-description">
-                    Descriprion: {description || "A software engineer based in lagos Nigeria"}
+                    Descriprion: {description}
                 </p>
-            </div>
+            </div>}
             <div className="repo-info">
                 <div className="repo-lang-license">
                     <p>Language: {language || "Java"}</p>
-                    <p>License: {license.spdx_id || "MIT"}</p>
+                    <p>License: {license?.spdx_id || "MIT"}</p>
                 </div>
 
                 <div className="repo-icons">
@@ -51,7 +175,6 @@ const RepoCard = ({ item }) => {
                     <span>
                         <GoOrganization />
                     </span>
-
 
                 </div>
             </div>
@@ -134,7 +257,7 @@ const StyledRepoCard = styled.div`
         width: 60px;
         height: 60px;
         margin-right: 15px;
-        background-image: url("./images/wallon.png");
+        background-image: ${({avatar}) => `url(${avatar})`};
         background-repeat: no-repeat;
         background-size: cover;
         border: 1px gray solid;
