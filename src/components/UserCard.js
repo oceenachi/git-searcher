908c078a5e0b6862207f5f4d5c3124df0c4f9a8a
francisca eze <phillipfrancisca@gmail.com>
2021-06-22 01:52:43 +0100

    set up repo card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,28 +4,32 @@ import { GoRepo } from "react-icons/go";
 import { BsBuilding, BsEnvelope, BsFileCode, BsLink45Deg, BsStar, BsPeople } from "react-icons/bs";
 import { VscTwitter } from "react-icons/vsc"
 import { useDispatch, useSelector } from 'react-redux';
-import { changeParams, getCall, getUsers, makeCall, setLoading } from '../redux/action';
+import { getUsers } from '../redux/action';
 import { ScaleLoader } from 'react-spinners';
 
 
-const UserCard = ({bio}) => {
-    const singleUser = useSelector((state) => state.usersReducer[bio.login]);
-
+const UserCard = ({ bio }) => {
     const [loadUser, setLoadUser] = useState(false)
 
+    const singleUser = useSelector((state) => state.usersReducer[bio.login]);
+
+
+    //effect hook to fetch users
     useEffect(() => {
-        if(singleUser === undefined){
-            dispatch(getUsers(bio.login, bio.url)).then(()=> {
+        if (singleUser === undefined) {
+            dispatch(getUsers(bio.login, bio.url)).then(() => {
                 setLoadUser(true)
-                console.log('sfsdfsfsfsd')
+                // console.log('sfsdfsfsfsd')
             });
-        }else{
+        } else {
             setLoadUser(true)
 
         }
 
     }, [])
-    console.log({singleUser});
+
+
+    // console.log({ singleUser });
 
     /*
         {
@@ -86,79 +90,81 @@ const UserCard = ({bio}) => {
 }
 
     */
-        const [fetchUser, setFetchUser] = useState(bio.url);
 
-        const [user, setUser] = useState({})
+    // const [fetchUser, setFetchUser] = useState(bio.url);
+
+    // const [user, setUser] = useState({})
 
 
-        const dispatch = useDispatch();
-    
-        // const handleChange = (e) => {
-        //     setFetchDetails({...fetchDetails, [e.target.name]: e.target.value})
-        // }
+    const dispatch = useDispatch();
 
-        useEffect(()=>{
+    // const handleChange = (e) => {
+    //     setFetchDetails({...fetchDetails, [e.target.name]: e.target.value})
+    // }
 
-        },[loadUser])
+    //custom hook to watch for user loaded states
+    useEffect(() => {
+
+    }, [loadUser])
 
     return (
         <StyledUserCard>
             <div className="full-card">
                 <div className="top-section">
                     <div className="avatar-box">
-                        <img src={`${(singleUser||bio).avatar_url}`} alt="" className="avatar"/>
+                        <img src={`${(singleUser || bio).avatar_url}`} alt="" className="avatar" />
                     </div>
                     <div className="brief-intro">
-                        <p className="info full-name">{(loadUser && singleUser.name) || " e"}</p>
-                        <p className="info login">{`@${(singleUser||bio).login}`}</p>
+                        <p className="info full-name">{(loadUser && singleUser.name) || "Not available"}</p>
+                        <p className="info login">{`@${(singleUser || bio).login || "Not available"}`}</p>
                         <p className="info bio" title={'bio'}>{
-                        loadUser && singleUser.bio && (singleUser.bio?.split(" ").length > 8 ? singleUser.bio?.split(" ").slice(0, 8).join(" ") + "..." : singleUser.bio)
-                  
+                            loadUser && singleUser.bio && (singleUser.bio?.split(" ").length > 15 ? singleUser.bio?.split(" ").slice(0, 15).join(" ") + "..." : singleUser.bio)
+
                         }</p>
-                        
+
                     </div>
                 </div>
             </div>
             <div>
-                { !loadUser? <ScaleLoader/>: <div className="bottom-section">
+                {!loadUser ? <ScaleLoader /> : <div className="bottom-section">
                     <hr />
                     <p title="Email" className="info email">
                         <span className="git-icon">
                             <BsEnvelope />
                         </span>
-                        {singleUser.email}
+                        {singleUser.email || "Not available"}
                     </p>
                     <p title="Personal Website" className="info website">
                         <span className="git-icon">
                             <BsLink45Deg />
                         </span>
-                        <a href={singleUser.blog} target="_blank">{singleUser.blog}</a>
+                        <a href={singleUser.blog} target="_blank">{singleUser.blog || "Not available"}</a>
                     </p>
                     <p title="Twitter" className="info twitter">
                         <span className="git-icon">
                             <VscTwitter />
                         </span>
-                        <a href={`https://twitter.com/${singleUser.twitter_username}`} target="_blank">{`@${singleUser.twitter_username}`}</a>
+                        <a href={`https://twitter.com/${singleUser.twitter_username}`} target="_blank">{`@${singleUser.twitter_username}` || "Not available"}</a>
                     </p>
                     <p title="Company" className="info Company">
                         <span className="git-icon">
                             <BsBuilding />
                         </span>
-                        {singleUser.company}
+                        {singleUser.company || "Not available"}
                     </p>
-                    <p className="info">{`Location: ${singleUser.location}`}</p>
+                    <p className="info">{`Location: ${singleUser.location}` || "Not available"}</p>
 
-                    <p className="info">{`Open To Hire: ${singleUser.hireable ? "Yes" : "No"}`}</p>
+                    <p className="info">{`Open To Hire: ${singleUser.hireable ? "Yes" : "No"}` || "Not available"}</p>
                 </div>}
                 <div className="activity-section">
                     <p title="Repositories"><span className="git-icon"><GoRepo /></span> 150</p>
-                    <p title="Github Gist"><span className="git-icon"><BsFileCode className="edit-icon"/></span> 10</p>
+                    <p title="Github Gist"><span className="git-icon"><BsFileCode className="edit-icon" /></span> 10</p>
                     <p title="Followers"><span className="git-icon"><BsPeople /></span> 10</p>
                     <p title="Following"><span className="git-icon"><BsPeople /></span> 10</p>
                     <p title="Github Star"><span className="git-icon"><BsStar /></span> 10</p>
                 </div>
             </div>
-   
+
         </StyledUserCard>
     )
 }
@@ -231,7 +237,6 @@ const StyledUserCard = styled.div`
         margin-right: .9rem;
     }
     .bottom-section{
-        /* border-top: 2px solid #2ea043; */
         padding: 1rem 1rem .5rem 1rem;
     }
     .activity-section{
