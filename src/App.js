1bf47ea33c4770db99fcd997a25da9a622ed1943
francisca eze <phillipfrancisca@gmail.com>
2021-06-22 01:55:42 +0100

    add new changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-import { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Provider } from "react-redux";
-import ParentBody from "./components/ParentBody";
 import { PersistGate } from "redux-persist/integration/react";
 import configStore from "./redux/configureStore";
-import {MoonLoader, ScaleLoader} from "react-spinners"
+import { MoonLoader, ScaleLoader } from "react-spinners";
 import { ToastContainer } from "react-toastify";
-import ErrorBoundary from './components/ErrorBoundary';
-
+import ErrorBoundary from "./components/ErrorBoundary";
+const ParentBody = React.lazy(() => import("./components/ParentBody"));
 
 function App() {
-
   //wrap store and persistor around app.js
-  
+
   return (
     <>
       <Provider store={configStore.store}>
-        <PersistGate loading={<ScaleLoader/>} persistor={configStore.persistor}>
-        <ErrorBoundary>
-        <Suspense fallback={<MoonLoader />}>
-        <ToastContainer style={{ fontSize: "16px" }} />
+        <PersistGate
+          loading={<ScaleLoader />}
+          persistor={configStore.persistor}
+        >
+          <ErrorBoundary>
+            <Suspense fallback={<MoonLoader />}>
+              <ToastContainer style={{ fontSize: "16px" }} />
 
-          <ParentBody />
-          
-          </Suspense>
-
+              <ParentBody />
+            </Suspense>
           </ErrorBoundary>
-    
-       
-
         </PersistGate>
-
-
       </Provider>
     </>
   );
